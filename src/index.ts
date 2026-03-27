34f925df6dee444786efced9255d000e88214070
Amantle Nthubu <161377098+Amantl3@users.noreply.github.com>
2026-03-27 06:13:59 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ dotenv.config();
 
 import app from './app';
 
+// Railway provides the PORT environment variable automatically
 const PORT = process.env.PORT || 8080;
 
 app.listen(Number(PORT), '0.0.0.0', () => {
